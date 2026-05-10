54baaf734ac709bf0fe3ce5e30df1ddd671dbf7d
dusanov <sadriddindusanov@gmail.com>
2026-05-10 22:41:05 +0500

    Started. All packages is created. tz is added to gitignore

---
+++ src/main/java/top/news/TopNewsApplication.java
@@ -0,0 +1,13 @@
+package top.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/news/TopNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package top.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
