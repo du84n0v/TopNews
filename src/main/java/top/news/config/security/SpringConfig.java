ae56dc272724f4a5379fffc0df735ca7fbba9b95
dusanov <sadriddindusanov@gmail.com>
2026-06-12 11:58:08 +0500

    SpringConfig dagi CustomUserDetailsService UserDetailsService ga ozgartirildi

--- src/main/java/top/news/config/security/SpringConfig.java
+++ src/main/java/top/news/config/security/SpringConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,7 +23,7 @@ import java.util.List;
 public class SpringConfig {
 
     @Autowired
-    private CustomUserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
 
     @Autowired
     private JwtAuthenticationFilter jwtTokenFilter;
